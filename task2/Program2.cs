d60ace9030749905d32b9702f7bcad0ab5d0970b
Your Name <you@example.com>
2023-12-11 00:52:03 +0300

    zadanye 2

---
+++ task2/Program2.cs
@@ -0,0 +1,19 @@
+﻿
+// Задача 2: Задайте строку, содержащую латинские 
+// буквы в обоих регистрах. 
+// Сформируйте строку, в которой все 
+// заглавные буквы заменены на строчные.
+
+
+// Простите я не совсем понял задачу и надеюсь я сделал правильно. Спасибо!
+
+string astring = "Hello World";
+
+string LowString (string str){
+    string lowedastring = string.Empty;
+    lowedastring = str.ToLower();
+    return lowedastring;
+}
+
+string lowedastring = LowString(astring);
+Console.WriteLine(lowedastring);
